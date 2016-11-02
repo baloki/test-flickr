8cdd9655876075e1423c8f82219df475687483eb
Phillip Vose <webmaster@loki7.co.uk>
2016-11-02 21:51:51 +0000

    Initial Commit
    Added in basic task runners and Travis CI support.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# test-flickr
+# test-flickr [![Build Status](https://travis-ci.org/baloki/test-flickr.svg?branch=master)](https://travis-ci.org/baloki/test-flickr) [![NSP Status](https://nodesecurity.io/orgs/loki7/projects/3d9ed86f-544d-4e7e-b689-ae88a71a1cf0/badge)](https://nodesecurity.io/orgs/loki7/projects/3d9ed86f-544d-4e7e-b689-ae88a71a1cf0)
+
 Sync with Flickr Public Photos
+
+## Getting Started
+- Clone the Repo
+- Run `npm install`
+- Run `gulp` to start the task runner
+- Enjoy!
---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var ftp = require('vinyl-ftp'),
+    gutil = require('gulp-util'),
+    minimist = require('minimist'),
+    args = minimist(process.argv.slice(2)),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    sassLint = require('gulp-sass-lint');
+
+var conn = ftp.create({
+  host: 'ftp.loki7.com',
+  user: args.user,
+  password: args.password,
+  log: gutil.log
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('assets/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('assets/css'));
+});
+
+// Lint Our Sass
+gulp.task('sass-lint', function() {
+  return gulp.src('assets/scss/*.scss')
+        .pipe(sassLint())
+        .pipe(sassLint.format())
+        .pipe(sassLint.failOnError())
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('assets/scss/*.scss', ['sass-lint', 'sass']);
+});
+
+// Travis CI Tests
+gulp.task('ci-tasks', ['sass-lint']);
+
+// Deploy Task
+gulp.task('deploy', ['sass', 'clean'], function() {
+  var remotePath = '/dragoncloud/dumpbox/webpages/flickr/';
+
+  var globs = [
+    './assets/**/*.css',
+    './assets/**/*.css.map',
+    './assets/**/*.jpg',
+    './assets/**/*.png',
+    './assets/**/*.svg',
+    './assets/**/*.eot',
+    './assets/**/*.ttf',
+    './assets/**/*.woff',
+    './assets/**/*.woff2',
+    './assets/**/*.ico',
+    './assets/**/*.js'
+  ]
+
+  gulp.src( globs, { base: '.', buffer: false } )
+    .pipe(conn.dest(remotePath));
+
+  globs = [
+    './site/**/*.*'
+  ]
+
+  gulp.src( globs, { base: '.', buffer: false } )
+    .pipe(conn.dest(remotePath));
+});
+
+// Remove Existing Directories on Remote Server
+gulp.task('clean', ['sass', 'clean-assets']);
+
+gulp.task('clean-assets', function(cb) {
+  conn.rmdir('/assets/', cb)
+});
+
+// Default Task
+gulp.task('default', ['sass-lint', 'sass', 'watch']);
